78a9357ad44658bb34cfbfb49686deb484c41bc3
germanrcuriel <germix@germix.net>
2020-04-05 14:53:15 +0200

    Add Redis optional config

--- src/config.js
+++ src/config.js
@@ -10,6 +10,26 @@ let config = {
     receivePort: 11000,
     sendPort: 12000
   },
+  REDIS: {
+    enabled: false,
+    host: '127.0.0.1',
+    port: 6379,
+    password: '',
+    publishChannel: 'udp2ws.events',
+    eventsToPublish: [
+      'car_info',
+      'chat',
+      'client_loaded',
+      'connection_closed',
+      'end_session',
+      'error',
+      'lap_completed',
+      'new_connection',
+      'new_session',
+      'session_info'
+    ],
+    subscribeChannel: 'udp2ws.commands.*'
+  },
   WEBSOCKETS: {
     origins: '',
     port: 30000,
